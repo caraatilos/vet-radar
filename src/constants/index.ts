fc75a29641e542e65ee1c712bb132932545ee929
caraatilos <caraatilos@gmail.com>
2020-01-13 23:05:55 +1300

    Added IDs in data set

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,21 +1,26 @@
 export const products = [
   {
+    id: 1,
     name: 'Sledgehammer',
     price: 125.76
   },
   {
+    id: 2,
     name: 'Axe',
     price: 190.51
   },
   {
+    id: 3,
     name: 'Bandsaw',
     price: 562.14
   },
   {
+    id: 4,
     name: 'Chisel',
     price: 13.9
   },
   {
+    id: 5,
     name: 'Hacksaw',
     price: 19.45
   }
